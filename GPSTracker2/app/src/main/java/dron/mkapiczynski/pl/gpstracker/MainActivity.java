72e26665be405ba89a02ddf23501cebb10260838
mkapiczy <mix.kap@gmail.com>
2016-01-07 15:31:16 +0100

    GPSTracker2 - przerzucenie się na Android Location API

--- GPSTracker2/app/src/main/java/dron/mkapiczynski/pl/gpstracker/MainActivity.java
+++ GPSTracker2/app/src/main/java/dron/mkapiczynski/pl/gpstracker/MainActivity.java
@@ -1,20 +1,19 @@
 package dron.mkapiczynski.pl.gpstracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -24,18 +23,21 @@ import de.tavendo.autobahn.WebSocketException;
 import de.tavendo.autobahn.WebSocketHandler;
 
 
-public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class MainActivity extends Activity implements LocationListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final int PLAy_SERVICES_RESOLUTION_REQUEST = 1000;
+
+    // GPS
+    private LocationManager locationManager;
+    private LocationProvider gpsSignalProvider;
     private Location mLastLocation;
 
-    private GoogleApiClient mGoogleApiClient;
-
+    // Flags
+    private boolean isGPSEnabled = false;
+    private boolean isNetworkEnabled = false;
     private boolean requestLocationUpdatesFlag = false;
 
-    private LocationRequest mLocationRequest;
-
+    // Gps parameters
     private static int UPDATE_INTERVAL = 3000; // 3 sec
     private static int FASTEST_INTERVAL = 1500; // 1,5sec
     private static int DISPLACEMENT = 1; // 1 meters
@@ -46,7 +48,7 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     private Button btnShowLocation, btnStartLocationUpdates;
 
     // Websocket
-    private static final String SERVER = "ws://0.tcp.ngrok.io:48269/dron-server-web/server";
+    private static final String SERVER = "ws://0.tcp.ngrok.io:45960/dron-server-web/server";
     private final WebSocketConnection client = new WebSocketConnection();
 
 
@@ -60,10 +62,10 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
         btnStartLocationUpdates = (Button) findViewById(R.id.btnLocationUpdates);
 
-        if (checkPlayServices()) {
-            buildGoogleApiClient();
-            createLocationRequest();
-        }
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        gpsSignalProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+
+        togglePeriodicLocationUpdates();
 
         connectToWebSocketServer();
 
@@ -117,7 +119,7 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
 
     private void sendGeoDataMessageToServer(){
         GeoDataMessage geoDataMessage = new GeoDataMessage();
-        geoDataMessage.setDeviceId("Device1");
+        geoDataMessage.setDeviceId("Device2");
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         geoDataMessage.setTimestamp(dateFormat.format(date.getTime()));
@@ -129,8 +131,14 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         }
     }
 
-    private Location getLastLocation(){
-        return LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    private Location getLastLocation() {
+        Location updatedLocation = new Location(gpsSignalProvider.getName());
+        try {
+            updatedLocation = locationManager.getLastKnownLocation(gpsSignalProvider.getName());
+        } catch (SecurityException e) {
+            Log.e(TAG, "Exception reading last known location " + e.toString());
+        }
+        return updatedLocation;
     }
 
     private void displayLocationOnUI() {
@@ -141,86 +149,58 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         }
     }
 
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API).build();
+    private void togglePeriodicLocationUpdates(){
+        if(!requestLocationUpdatesFlag){
+            btnStartLocationUpdates
+                    .setText(getString(R.string.btn_stop_location_updates));
 
-    }
+            requestLocationUpdatesFlag = true;
 
-    private boolean checkPlayServices() {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAy_SERVICES_RESOLUTION_REQUEST).show();
-            } else {
-                Toast.makeText(getApplicationContext(), "This device is not supported", Toast.LENGTH_LONG).show();
-                finish();
+            try {
+                locationManager.requestLocationUpdates(gpsSignalProvider.getName(), 500, 0, this);
+            } catch (SecurityException e) {
+                Log.e("GPS", "Exception setting request Location Updates properties of LocationManager object " + e.toString());
             }
-            return false;
-        }
-        return true;
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.connect();
-        }
+            Log.d(TAG, "Periodic location updates started!");
+        } else{
+            // Changing the button text
+            btnStartLocationUpdates
+                    .setText(getString(R.string.btn_start_location_updates));
 
-    }
+            requestLocationUpdatesFlag = false;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+            // Stopping the location updates
+            try {
+                locationManager.removeUpdates(this);
+            } catch (SecurityException e){
+                Log.e("GPS", "Exception removing Location Updates of LocationManager object " + e.toString());
+            }
 
-        checkPlayServices();
-
-        // Resuming the periodic location updates
-        if (mGoogleApiClient.isConnected() && requestLocationUpdatesFlag) {
-            startLocationUpdates();
-        }
-
-        if(!client.isConnected() || client==null){
-            connectToWebSocketServer();
+            Log.d(TAG, "Periodic location updates stopped!");
         }
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    public void onProviderEnabled(String provider){
+
     }
 
     @Override
-    public void onConnected(Bundle bundle) {
-        // When connecion is established getLastLocation and display it
-        mLastLocation = getLastLocation();
-        displayLocationOnUI();
-
-        if (requestLocationUpdatesFlag) {
-            startLocationUpdates();
+    public void onProviderDisabled(String provider) {
+        if (LocationManager.GPS_PROVIDER.equals(provider)) {
+            Toast.makeText(getApplicationContext(), "GPS wyłączony. Sprawdź ustawienia", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+        } else if (LocationManager.NETWORK_PROVIDER.equals(provider)) {
+            Toast.makeText(getApplicationContext(), "Dostawca internetu odłączony. Sprawdź ustawienia", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS));
         }
     }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        mGoogleApiClient.connect();
-    }
-
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
-                + connectionResult.getErrorCode());
-    }
-
     @Override
     public void onLocationChanged(Location location) {
 
@@ -241,60 +221,4 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     }
 
 
-    /**
-     * Method to toggle periodic location updates
-     */
-    private void togglePeriodicLocationUpdates() {
-        if (!requestLocationUpdatesFlag) {
-            // Changing the button text
-            btnStartLocationUpdates
-                    .setText(getString(R.string.btn_stop_location_updates));
-
-            requestLocationUpdatesFlag = true;
-
-            // Starting the location updates
-            startLocationUpdates();
-
-            Log.d(TAG, "Periodic location updates started!");
-
-        } else {
-            // Changing the button text
-            btnStartLocationUpdates
-                    .setText(getString(R.string.btn_start_location_updates));
-
-            requestLocationUpdatesFlag = false;
-
-            // Stopping the location updates
-            stopLocationUpdates();
-
-            Log.d(TAG, "Periodic location updates stopped!");
-        }
-    }
-
-    /**
-     * Creating location request object
-     */
-    protected void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(UPDATE_INTERVAL);
-        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-        mLocationRequest.setSmallestDisplacement(DISPLACEMENT); // 10 meters
-    }
-
-    /**
-     * Starting the location updates
-     */
-    protected void startLocationUpdates() {
-        LocationServices.FusedLocationApi.requestLocationUpdates(
-                mGoogleApiClient, mLocationRequest, this);
-    }
-
-    /**
-     * Stopping location updates
-     */
-    protected void stopLocationUpdates() {
-        LocationServices.FusedLocationApi.removeLocationUpdates(
-                mGoogleApiClient, this);
-    }
 }
