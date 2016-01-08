64fe7fe03888cd3c769f421e2ea31cca427a970b
mkapiczy <mix.kap@gmail.com>
2016-01-08 15:12:21 +0100

    dron-sever - Mój GeoPoint

---
+++ dron-server/dron-server/dron-server-ejb/src/main/java/domain/GeoPoint.java
@@ -0,0 +1,92 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class GeoPoint {
+	public static final float DEG2RAD = (float) (Math.PI / 180.0);
+	public static final float RAD2DEG = (float) (180.0 / Math.PI);
+	
+	public static final int RADIUS_EARTH_METERS = 6378137; // http://en.wikipedia.org/wiki/Earth_radius#Equatorial_radius
+	public static final double METERS_PER_STATUTE_MILE = 1609.344; // http://en.wikipedia.org/wiki/Mile
+	public static final double METERS_PER_NAUTICAL_MILE = 1852; // http://en.wikipedia.org/wiki/Nautical_mile
+	public static final double FEET_PER_METER = 3.2808399; // http://en.wikipedia.org/wiki/Feet_%28unit_of_length%29
+	public static final int EQUATORCIRCUMFENCE = (int) (2 * Math.PI * RADIUS_EARTH_METERS);
+	
+	private double latitude;
+	private double longitude;
+	private double altitude;
+
+	public GeoPoint() {
+
+	}
+	
+	public GeoPoint(double latitude, double longitude){
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	public GeoPoint(double latitude, double longitude, double altitude) {
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.altitude = altitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getAltitude() {
+		return altitude;
+	}
+
+	public void setAltitude(double altitude) {
+		this.altitude = altitude;
+	};
+	
+	public GeoPoint destinationPoint(final double aDistanceInMeters, final float aBearingInDegrees) {
+
+		// convert distance to angular distance
+		final double dist = aDistanceInMeters / RADIUS_EARTH_METERS;
+
+		// convert bearing to radians
+		final float brng = DEG2RAD * aBearingInDegrees;
+
+		// get current location in radians
+		final double lat1 = DEG2RAD * getLatitude() / 1E6;
+		final double lon1 = DEG2RAD * getLongitude() / 1E6;
+
+		final double lat2 = Math.asin(Math.sin(lat1) * Math.cos(dist) + Math.cos(lat1)
+				* Math.sin(dist) * Math.cos(brng));
+		final double lon2 = lon1
+		+ Math.atan2(Math.sin(brng) * Math.sin(dist) * Math.cos(lat1), Math.cos(dist)
+				- Math.sin(lat1) * Math.sin(lat2));
+
+		final double lat2deg = lat2 / DEG2RAD;
+		final double lon2deg = lon2 / DEG2RAD;
+
+		return new GeoPoint(lat2deg, lon2deg);
+	}
+	
+	public static ArrayList<GeoPoint> pointsAsCircle(GeoPoint center, double radiusInMeters) {
+        ArrayList circlePoints = new ArrayList(60);
+
+        for(int f = 0; f < 360; f += 6) {
+            GeoPoint onCircle = center.destinationPoint(radiusInMeters, (float)f);
+            circlePoints.add(onCircle);
+        }
+
+        return circlePoints;
+    }
+
+}
